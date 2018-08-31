5a5a3eb07702a9412aee024954a9c07263cbecd7
Jesper Veggerby <jesper@veggerby.com>
2018-08-31 14:34:13 +0200

    added game state comparison

--- src/Veggerby.Boards.Core/GameEngine.cs
+++ src/Veggerby.Boards.Core/GameEngine.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Veggerby.Boards.Core.Artifacts;
 using Veggerby.Boards.Core.Flows.Events;
 using Veggerby.Boards.Core.Flows.Phases;
+using Veggerby.Boards.Core.Flows.Rules;
 using Veggerby.Boards.Core.States;
 
 namespace Veggerby.Boards.Core
@@ -16,6 +17,27 @@ namespace Veggerby.Boards.Core
         public IEnumerable<IGameEvent> Events => _events.ToList().AsReadOnly();
 
         private readonly IList<IGameEvent> _events = new List<IGameEvent>();
+
+/*        public GameState HandleEvent(IGameEvent @event)
+        {
+            var gamePhase = GamePhaseRoot.GetActiveGamePhase(GameState);
+            var ruleCheck = gamePhase.Rule.Check(GameState, @event);
+            if (ruleCheck.Result == RuleCheckResult.Valid)
+            {
+                var newState = gamePhase.Rule.HandleEvent(GameState, @event);
+                if (newState.Equals(GameState))
+                {
+                    // NOOP, do not record event
+                    return GameState;
+                }
+
+                _events.Add(@event);
+
+                GameState = newState;
+            }
+
+            return GameState;
+        }*/
 
         private GameEngine(GameState initialState, GamePhase gamePhaseRoot)
         {
